0f16c027bdf1059c7605d9026707db89104d2ee6
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 23:32:18 +0200

    Register NoopConverter

--- src/test/java/io/github/mklew/geda/autodiscovery/domain/NoopConverter.java
+++ src/test/java/io/github/mklew/geda/autodiscovery/domain/NoopConverter.java
@@ -2,11 +2,13 @@ package io.github.mklew.geda.autodiscovery.domain;
 
 import com.inspiresoftware.lib.dto.geda.adapter.BeanFactory;
 import com.inspiresoftware.lib.dto.geda.adapter.ValueConverter;
+import io.github.mklew.geda.autodiscovery.annotations.RegisterAs;
 
 /**
  * @author Marek Lewandowski
  * @since 7/27/13
  */
+@RegisterAs("noopConverterKey")
 public class NoopConverter implements ValueConverter
 {
     @Override
