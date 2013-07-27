8fc3fd2a3ab2636afc371811b2d9bde8ef47161e
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 21:45:00 +0200

    Make test compile

--- src/main/java/io/github/mklew/geda/autodiscovery/RegisterAsAnnotationScanner.java
+++ src/main/java/io/github/mklew/geda/autodiscovery/RegisterAsAnnotationScanner.java
@@ -1,9 +1,30 @@
 package io.github.mklew.geda.autodiscovery;
 
+import java.util.Collection;
+
 /**
  * @author Marek Lewandowski
  * @since 7/27/13
  */
 public class RegisterAsAnnotationScanner
 {
+    private String basePackage;
+
+    public RegisterAsAnnotationScanner(String basePackage)
+    {
+        this.basePackage = basePackage;
+    }
+
+
+    public void scan()
+    {
+
+
+    }
+
+    public Collection<Class<?>> getClassesAnnotatedWithRegisterAs()
+    {
+         return null;
+
+    }
 }
