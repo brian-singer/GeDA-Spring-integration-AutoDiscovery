ea52a308a711cf173e56191a907c2e53e7525181
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 22:53:05 +0200

    Implement AutoDiscoveryDtoFactoryImpl

--- src/main/java/io/github/mklew/geda/autodiscovery/AutoDiscoveryDTOFactoryImpl.java
+++ src/main/java/io/github/mklew/geda/autodiscovery/AutoDiscoveryDTOFactoryImpl.java
@@ -1,9 +1,34 @@
 package io.github.mklew.geda.autodiscovery;
 
+import com.inspiresoftware.lib.dto.geda.impl.DTOFactoryImpl;
+import io.github.mklew.geda.autodiscovery.internal.DtoEntitySplitterImpl;
+import io.github.mklew.geda.autodiscovery.internal.RegisterAsAnnotationScannerImpl;
+
+import javax.annotation.PostConstruct;
+
 /**
  * @author Marek Lewandowski
  * @since 7/27/13
  */
-public class AutoDiscoveryDTOFactoryImpl
+public class AutoDiscoveryDTOFactoryImpl extends DTOFactoryImpl
 {
+    private String basePackage;
+
+    public AutoDiscoveryDTOFactoryImpl(String basePackage)
+    {
+        this.basePackage = basePackage;
+    }
+
+    @PostConstruct
+    void registerDtosAndEntities()
+    {
+        DtoEntitySplitter splitter = new DtoEntitySplitterImpl();
+        RegisterAsAnnotationScanner scanner = new RegisterAsAnnotationScannerImpl(basePackage);
+        DtoEntityRegistrar registrar = new DtoEntityRegistrar();
+
+        registrar.setDtoFactory(this);
+        registrar.setRegisterAsAnnotationScanner(scanner);
+        registrar.setSplitter(splitter);
+        registrar.registerDtoAndEntites();
+    }
 }
