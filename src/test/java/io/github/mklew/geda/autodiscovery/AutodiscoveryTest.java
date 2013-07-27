139050cbd2550253b6a336578f78a158681a95d6
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 22:37:15 +0200

    Add another test. All pass

--- src/test/java/io/github/mklew/geda/autodiscovery/AutodiscoveryTest.java
+++ src/test/java/io/github/mklew/geda/autodiscovery/AutodiscoveryTest.java
@@ -4,6 +4,7 @@ import com.inspiresoftware.lib.dto.geda.DTOSupport;
 import io.github.mklew.geda.autodiscovery.domain.Person;
 import io.github.mklew.geda.autodiscovery.domain.impl.PersonImpl;
 import io.github.mklew.geda.autodiscovery.dto.PersonDto;
+import io.github.mklew.geda.autodiscovery.dto.impl.PersonDtoImpl;
 import org.fest.assertions.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -35,6 +36,24 @@ public class AutodiscoveryTest  extends AbstractTestNGSpringContextTests
         // then
         Assertions.assertThat(personDto.getFirstName()).isEqualTo(person.getFirstName());
         Assertions.assertThat(personDto.getLastName()).isEqualTo(person.getLastName());
+    }
+
+    @Test
+    public void should_assemble_entity_using_dto()
+    {
+        // given
+        PersonDto personDto = new PersonDtoImpl();
+        personDto.setFirstName("John");
+        personDto.setLastName("Doe");
+
+        // when
+        final Object oPerson = dtoSupport.assembleEntityByKey(personDto, "personEntityKey", null);
+        Person person = (Person) oPerson;
+        personDto.setLastName("Doe modified");
+        // then
+        Assertions.assertThat(person.getFirstName()).isEqualTo("John");
+        Assertions.assertThat(person.getLastName()).isEqualTo("Doe");
+
     }
 
 }
