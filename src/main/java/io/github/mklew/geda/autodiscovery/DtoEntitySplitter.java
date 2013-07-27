d44a1dae284369f994bdc676fbaa9ce535d2505c
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 22:01:21 +0200

    Add DtoEntitySplitter class

---
+++ src/main/java/io/github/mklew/geda/autodiscovery/DtoEntitySplitter.java
@@ -0,0 +1,15 @@
+package io.github.mklew.geda.autodiscovery;
+
+/**
+ * @author Marek Lewandowski
+ * @since 7/27/13
+ */
+public class DtoEntitySplitter
+{
+    private RegisterAsAnnotationScanner registerAsAnnotationScanner;
+
+    public void setRegisterAsAnnotationScanner(RegisterAsAnnotationScanner registerAsAnnotationScanner)
+    {
+        this.registerAsAnnotationScanner = registerAsAnnotationScanner;
+    }
+}
