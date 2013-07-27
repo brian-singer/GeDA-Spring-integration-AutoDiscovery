7827b7cbf78c22b031870686e5774a40e07bddb8
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 22:36:51 +0200

    Implement DtoEntityRegistrar

--- src/main/java/io/github/mklew/geda/autodiscovery/DtoEntityRegistrar.java
+++ src/main/java/io/github/mklew/geda/autodiscovery/DtoEntityRegistrar.java
@@ -1,9 +1,49 @@
 package io.github.mklew.geda.autodiscovery;
 
+import com.inspiresoftware.lib.dto.geda.DTOFactory;
+import io.github.mklew.geda.autodiscovery.annotations.RegisterAs;
+import io.github.mklew.geda.autodiscovery.annotations.RepresentAs;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.Set;
+
 /**
  * @author Marek Lewandowski
  * @since 7/27/13
  */
 public class DtoEntityRegistrar
 {
+    @Autowired
+    DTOFactory dtoFactory;
+
+    @Autowired
+    DtoEntitySplitter splitter;
+
+    @Autowired
+    RegisterAsAnnotationScanner registerAsAnnotationScanner;
+
+    @PostConstruct
+    public void registerDtoAndEntites()
+    {
+        registerAsAnnotationScanner.scan();
+        final Set<Class<?>> classesAnnotatedWithRegisterAs = registerAsAnnotationScanner.getClassesAnnotatedWithRegisterAs();
+        final DtosAndEntites dtosAndEntites = splitter.split(classesAnnotatedWithRegisterAs);
+
+        for(Class<?> clazz : dtosAndEntites.getDtos())
+        {
+            final RegisterAs annotation = clazz.getAnnotation(RegisterAs.class);
+            dtoFactory.registerDto(annotation.value(), clazz.getName());
+        }
+
+        for(Class<?> clazz : dtosAndEntites.getEntities())
+        {
+            Class<?> representAs = null;
+            if (clazz.isAnnotationPresent(RepresentAs.class))
+                representAs = clazz.getAnnotation(RepresentAs.class).value();
+            else
+                representAs = clazz;
+            dtoFactory.registerEntity(clazz.getAnnotation(RegisterAs.class).value(), clazz.getName(), representAs.getCanonicalName());
+        }
+    }
 }
