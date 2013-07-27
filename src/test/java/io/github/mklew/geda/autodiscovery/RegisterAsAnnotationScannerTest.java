53b9d88912871d05d0e8c0b62dae5e2c11dc9d0a
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 21:44:47 +0200

    Write failing test

--- src/test/java/io/github/mklew/geda/autodiscovery/RegisterAsAnnotationScannerTest.java
+++ src/test/java/io/github/mklew/geda/autodiscovery/RegisterAsAnnotationScannerTest.java
@@ -1,9 +1,48 @@
 package io.github.mklew.geda.autodiscovery;
 
+import io.github.mklew.geda.autodiscovery.domain.impl.PersonImpl;
+import org.fest.assertions.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.util.MatcherAssertionErrors;
+import org.testng.annotations.Test;
+
+import java.util.Collection;
+
+
 /**
  * @author Marek Lewandowski
  * @since 7/27/13
  */
-public class RegisterAsAnnotationScannerTest
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+public class RegisterAsAnnotationScannerTest extends AbstractTestNGSpringContextTests
 {
+    @Configuration
+    public static class Config {
+
+        @Bean
+        public RegisterAsAnnotationScanner registerAsAnnotationScanner()
+        {
+            return new RegisterAsAnnotationScanner("io.github.mklew.geda.autodiscovery.domain");
+        }
+
+    }
+
+    @Autowired
+    RegisterAsAnnotationScanner registerAsAnnotationScanner;
+
+    @Test
+    public void should_discover_entity()
+    {
+        // when
+        registerAsAnnotationScanner.scan();
+        Collection<Class<?>> classes = registerAsAnnotationScanner.getClassesAnnotatedWithRegisterAs();
+
+        // then
+        Assertions.assertThat(classes).isNotEmpty().hasSize(1).contains(PersonImpl.class);
+    }
 }
