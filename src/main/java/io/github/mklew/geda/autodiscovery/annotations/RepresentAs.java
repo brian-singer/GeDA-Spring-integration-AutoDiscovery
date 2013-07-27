b60b7b9b3d1344f2370eb9e3089092eceb4ba949
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-27 22:35:45 +0200

    Implement RepresentAs annotation

--- src/main/java/io/github/mklew/geda/autodiscovery/annotations/RepresentAs.java
+++ src/main/java/io/github/mklew/geda/autodiscovery/annotations/RepresentAs.java
@@ -1,9 +1,15 @@
 package io.github.mklew.geda.autodiscovery.annotations;
 
+import java.lang.annotation.*;
+
 /**
  * @author Marek Lewandowski
  * @since 7/27/13
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface RepresentAs
 {
+    Class<?> value();
 }
