c4c11a1e18932b246d8cb47648ed7205ce685f05
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-07-28 17:09:41 +0200

    Provide base package

--- src/main/java/io/github/mklew/geda/autodiscovery/AutodiscoveryMappingAdapterRegistrar.java
+++ src/main/java/io/github/mklew/geda/autodiscovery/AutodiscoveryMappingAdapterRegistrar.java
@@ -11,14 +11,18 @@ import java.util.Map;
  */
 public class AutodiscoveryMappingAdapterRegistrar extends MappingAdapterRegistrar
 {
-    public AutodiscoveryMappingAdapterRegistrar()
+    private final String[] basePackages;
+
+    public AutodiscoveryMappingAdapterRegistrar(String... basePackages)
     {
         super(Collections.<String, Object>emptyMap());
+        this.basePackages = basePackages;
     }
 
     @Override
     protected Map<String, Object> getAdapters()
     {
+        // TODO implement this
         return super.getAdapters();    //To change body of overridden methods use File | Settings | File Templates.
     }
 }
